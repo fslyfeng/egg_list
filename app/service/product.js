0cbde55bba14c7062aa072ba23ce1535531f205d
netren <netren@netrendeiMac.local>
2020-09-20 16:53:57 +0800

    add umi 前台在 client-demo

--- app/service/product.js
+++ app/service/product.js
@@ -5,7 +5,7 @@ const Service = require('egg').Service;
 class ProductService extends Service {
   async findProducts(keyword) {
     const client = this.app.mysql;
-    const sql = 'select id,name from product';
+    const sql = 'select id,name,price,img_url from product';
     if (!keyword) {
       return await client.query(sql);
     } else if (keyword) {
---
+++ command.md
@@ -0,0 +1,21 @@
+# 安装使用命令记录
+
+## 安装 egg 后台和其中插件
+
+    npm init egg --type=simple
+    yarn add egg-mysql
+    yarn add egg-mssql
+    yarn add dotenv
+
+## 运行后台
+
+    yarn dev
+
+## 安装 umi 前台
+
+    yarn create @umijs/umi-app
+    yarn
+
+## 运行前台
+
+    yarn start
